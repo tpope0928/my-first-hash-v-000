9573d7f9436fe496db7f3454b95280437a74e6d2
Thomas Pope <tpope0928@gmail.com>
2018-08-23 00:46:30 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,9 +4,7 @@ end
 
 
 def shipping_manifest
-  shipping_manifest = {"whale bone corsets" => "5", 
-
-
+  shipping_manifest = {"whale bone corsets" => "5", "porcelain vases" => "2", "oil paintings" => "3"}
 end
 
 def retrieval

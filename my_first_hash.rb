eaccac52a002daec5df4482c8784f083d500beb4
Thomas Pope <tpope0928@gmail.com>
2018-08-23 00:51:51 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -26,7 +26,7 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-shipping_manifest["musket"]
+shipping_manifest["musket"] = 
 
   # add 4 gun powder to the shipping_manifest hash below
 
